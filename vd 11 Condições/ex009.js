728b680c8a27f430274983e89cc7c369af336328
Fernanda Monteiro Castellan <f3castellan@gmail.com>
2025-08-03 21:49:36 -0300

    novo exercicio

---
+++ vd 11 Condições/ex009.js
@@ -0,0 +1,23 @@
+
+//condição composta 
+
+/*var pais = 'Brasil'
+
+
+console.log( `Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log(' Você é Estrangeiro!')
+}*/
+
+// ou com !=
+
+var pais = 'Brasil'
+
+console.log( `Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+console.log(' Você é Estrangeiro!')
+} else {
+console.log('Você é Brasileiro!')
+}
